9bdbdbfcfed40387c80b94bc1d6cf844d00b06d9
AnnaShev <anreich0@gmail.com>
2023-08-19 14:01:50 +0300

    Initial commit

---
+++ src/main/java/ru/netology/qajava/javaqaarrays/services/StatsService.java
@@ -0,0 +1,66 @@
+package ru.netology.qajava.javaqaarrays.services;
+
+public class StatsService {
+    public int sumSales(long[] sales) {
+        int sumSales = 0; // сумма всех продаж
+
+        for (int i = 0; i < sales.length; i++) {
+            sumSales = (int) (sales[i] + sumSales);
+        }
+
+            return sumSales;
+    }
+
+    public int averageAmount(long[] sales) {
+        int averageAmount = sumSales(sales) / sales.length;
+        return averageAmount;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0; // номер месяца с максимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+
+        return maxMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                minMonth = i; // запомним его как минимальный
+            }
+        }
+
+        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+    }
+
+    public int underAverageAmount(long[] sales) {
+        int underAverageAmount = 0; // количество месяцев, в которых продажи были ниже среднего
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < averageAmount(sales)) {
+                underAverageAmount = underAverageAmount + 1;
+            }
+        }
+
+        return underAverageAmount;
+    }
+
+    public int upperAverageAmount(long[] sales) {
+        int upperAverageAmount = 0; // количество месяцев, в которых продажи были выше среднего
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > averageAmount(sales)) {
+                upperAverageAmount = upperAverageAmount + 1;
+            }
+        }
+
+        return upperAverageAmount;
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/javaqaarrays/services/StatsServiceTest.java
@@ -0,0 +1,79 @@
+package ru.netology.javaqa.javaqaarrays.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.qajava.javaqaarrays.services.StatsService;
+
+public class StatsServiceTest {
+    @Test
+    public void sumOfAllSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expected = 180;
+        int actual = service.sumSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+         }
+
+    @Test
+    public void calculateAverageAmountOfSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expected = 15;
+        int actual = service.averageAmount(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMonthOfMaxSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expected = 8;
+        int actual = service.maxSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getMonthOfMinSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expected = 9;
+        int actual = service.minSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void quantityOfSalesUnderAverageAmount() {
+        StatsService service = new StatsService();
+
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expected = 5;
+        int actual = service.underAverageAmount(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void quantityOfSalesUpperAverageAmount() {
+        StatsService service = new StatsService();
+
+        long[] sales = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expected = 5;
+        int actual = service.upperAverageAmount(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
